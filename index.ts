93a462e93a8b86af5a35da658d3a3846145e6362
paulandreacl <paula150995@hotmail.com>
2021-11-11 07:08:03 -0500

    creacion de modelo user, crear y obtener usuarios en mongo

--- index.ts
+++ index.ts
@@ -1,7 +1,32 @@
 import conectarBD from './db/db';
+import { UserModel } from './models/user';
+import { Enum_Rol } from './models/enums';
 
 const main = async () => {
     await conectarBD();
+
+    //CREAR USUARIO
+    await UserModel.create({
+        apellido: "Calderon",
+        correo: "paula150995@hotmail.com",
+        identificacion: "12345",
+        nombre: "Paula",
+        rol: Enum_Rol.administrador,
+    })
+        .then((u) => {
+            console.log("Usuario creado", u);
+        })
+        .catch((e) => {
+            console.log("Error creando el usuario", e);
+        });
+
+    //OBTENER LOS USUARIO
+    await UserModel.find().then((u) => {
+        console.log("usuarios", u);
+    })
+        .catch((e) => {
+            console.log("error obteniendo los datos"), e;
+        });
 };
 
 main();
